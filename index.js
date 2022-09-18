dcd12205b738880161cf8a44aa72a84e5e9629ea
Nishaojha <its.nishaojha@gmail.com>
2022-09-18 12:27:19 +0530

    Setting up view Engine ejs

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const port = 8000;
 
 // use express router
-app.use('/', require('./routes'))
+app.use('/', require('./routes'));
+// setting up view engine
+app.use('viewengine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function(err){
     if(err){
