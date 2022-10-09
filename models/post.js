9fd2ad10c4f1d154a800c5973f87315a8b85bf56
Nishaojha <its.nishaojha@gmail.com>
2022-10-09 20:16:55 +0530

    comments ended

--- models/post.js
+++ models/post.js
@@ -15,7 +15,7 @@ const postSchema = new mongoose.Schema({
     // include the array of ids of all comments in this post schema itself so that it loads fast
     comments: [{
         type:  mongoose.Schema.Types.ObjectId,
-        ref: 'comment'
+        ref: 'Comment'
     }]
 },{
     timestamps: true
