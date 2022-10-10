b49be4e1eb853a55a261fd8a06179bbceafd2afa
Nishaojha <its.nishaojha@gmail.com>
2022-10-10 11:58:01 +0530

    Deleting an authorized comment

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 // this is going tp control multiple users
 module.exports.post = function(req,res){
@@ -16,4 +17,22 @@ module.exports.create = function(req, res){
         return res.redirect('back');
     });
 
+}
+
+// creating a destroy action
+module.exports.destroy = function(req, res){
+    // we will be making (/posts/destroy/id) route and destroy the id, Id is the string param
+    Post.findById(req.params.id, function(err, post){
+        // Need to check whether the user who is deleting the post is the user who has written the post
+        // .id means converting the object id into string
+        if(post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id}, function(err){
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
+    })
 }
