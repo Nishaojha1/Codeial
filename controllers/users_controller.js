fb1a0c75300f13d5e9bbb916083b04ab194f0ef4
Nishaojha <its.nishaojha@gmail.com>
2022-10-14 19:10:27 +0530

    Saving file from params

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -14,14 +14,43 @@ module.exports.profile = function(req,res){
 }
 
 
-module.exports.update = function(req, res){
+module.exports.update = async function(req, res){
+    // if(req.user.id == req.params.id){
+    //     User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+    //         return res.redirect('back');
+    //     });
+    // }else{
+    //     return res.status(401).send('Unauthorized');
+    // }
+
     if(req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+        try {
+            // find the user 
+            let user = await User.findById(req.params.id);
+            // when we want to access the body params in the form, we wont be able to access directly from body.params because its a multi part form, For that multer has been deployed to perform the task(models/users  uploadedAvatar and avartarPath)
+            User.uploadedAvatar(req, res, function(err){
+                if(err){ console.log('*****Multer Error: ', err)}
+                user.name = req.body.name;
+                user.email = req.body.email;
+
+                // it will check if the user is not uploading a file then we are going to update only when the user will do it
+                if(req.file){
+                    // this is saving the path of the uploaded file into the avatar feild in the user
+                    user.avatar = User.avatarPath + '/' + req.file.filename
+                }
+                user.save();
+                return res.redirect('back');
+                // console.log(req.file);
+            });
+        } catch (err) {
+            req.flash('error', err);
             return res.redirect('back');
-        });
+        }
     }else{
-        return res.status(401).send('Unauthorized');
+        req.flash('error', 'Unauthorized')
+         return res.status(401).send('Unauthorized');
     }
+    
 }
 
 // render the sign up page
--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatars');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -13,11 +16,33 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    },
+    avatar: {
+        type: String
     }
 },{
     // timestamp is created at and updated at
     timestamps: true
 });
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '..', AVATAR_PATH));
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  });
+//   static functions(In OOPS static functions are the once that can be called overall on the whole class)
+userSchema.statics.uploadedAvatar = multer({
+  // single means only one instance or only one file will be send to avatar not multiple files
+  storage: storage}).single('avatar');
+
+userSchema.statics.avatarPath = AVATAR_PATH;
+  
+const upload = multer({ storage: storage });
+  
 
 const User = mongoose.model('User', userSchema);
 
